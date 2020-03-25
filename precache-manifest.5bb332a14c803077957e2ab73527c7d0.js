10560a25325c9a6aa0c6977a38acc0a58f80b385
unknown <vladislav.puzzyrev@gmail.com>
2020-03-26 01:17:39 +0500

    Updates

---
+++ precache-manifest.5bb332a14c803077957e2ab73527c7d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "609864f7b5225308963471a73bb1f41b",
+    "url": "/ideco-test/index.html"
+  },
+  {
+    "revision": "14e29d1967e077a07529",
+    "url": "/ideco-test/static/css/main.4076f4b9.chunk.css"
+  },
+  {
+    "revision": "b17fcfdcc691a0f2d226",
+    "url": "/ideco-test/static/js/2.9e74cb74.chunk.js"
+  },
+  {
+    "revision": "32fd9856557cf85053f70411a5ee6e8d",
+    "url": "/ideco-test/static/js/2.9e74cb74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14e29d1967e077a07529",
+    "url": "/ideco-test/static/js/main.012cbfd6.chunk.js"
+  },
+  {
+    "revision": "b3d06857024c1fec9e9e",
+    "url": "/ideco-test/static/js/runtime-main.f4c1344a.js"
+  }
+]);
